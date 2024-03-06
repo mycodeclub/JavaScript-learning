3d8e111d1e896f2a223e9ee7e3e8de09e3915990
MyCodeClub <ankitsahay1990@gmail.com>
2024-03-06 12:21:20 +0000

    added variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId =3445;
+const accountEmail ="ankit@gmail.com";
+let letVariable =34;
+var accId =43;
+console.log(accountId);
+ console.table(['ankit','sahay',19,'feb',accountId,letVariable,accId]);
+
